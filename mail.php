bf2e5048a9d297b4eed47f9b195297f32e27f291
Makwan BK <contact@makwanbk.com>
2020-11-30 16:03:53 +0300

    .

--- mail.php
+++ mail.php
@@ -1,15 +1,12 @@
 <?php
-	
-	if (isset($_POST['submit'])) {
-        $name = $_POST['name'];
-        $subject = $_POST['subject'];
-        $mailForm = $_POST['email'];
-        $message = $_POST['message'];
-
-        $mailTo = "contact@makwanbk.com";
-        $headers = "Form: ".$mailForm;
-
-        mail($mailTo, $subject, $txt, $headers);
-        header("Location: index.html?mailsend");
-    }
+$name = $_POST['name'];
+$email = $_POST['email'];
+$dropdown = $POST['subject'];
+$message = $_POST['message'];
+$formcontent="From: $name \n Message: $message";
+$recipient = "contact@makwanbk.com";
+$subject = "Contact Form";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Thank You!";
 ?>
